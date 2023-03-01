4aa3ff3be2eb813b81a5d2ee4c58ca819722fe14
LucianoRomero1 <lucianooromero1@gmail.com>
2023-03-01 12:31:02 -0300

    band,bands model, uuid, listen sockets

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,22 @@
 const { io } = require("../index");
+const Band = require("../model/band");
+const Bands = require("../model/bands");
 //Lo busco del index porque en ese archivo lo exporté
+
+const bands = new Bands();
+
+bands.addBand(new Band("Linkin Park"));
+bands.addBand(new Band("Nirvana"));
+bands.addBand(new Band("Metallica"));
+bands.addBand(new Band("System Of A Down"));
+
+console.log(bands);
 
 //Sockets
 io.on("connection", (client) => {
   console.log("Connected client");
+
+  client.emit("active-bands", bands.getBands());
 
   client.on("disconnect", () => {
     console.log("Disconnected client");
@@ -13,5 +26,10 @@ io.on("connection", (client) => {
     console.log("Message!", payload);
 
     io.emit("message", { admin: "New message" });
+  });
+
+  client.on("emit-message", (payload) => {
+    //io.emit("new-message", payload); emite a TODOS
+    client.broadcast.emit("new-message", payload); //Emite a todos menos el que lo emitio
   });
 });
